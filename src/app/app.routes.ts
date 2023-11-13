3b5959381e729a845c6a2f5394c4cc4b57cbf0bb
rijantp <rijanriju22@gmail.com>
2023-11-13 23:22:45 +0530

    added prettier, added auth&dashboard components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./auth/auth.component').then((m) => m.AuthComponent),
+  },
+]
